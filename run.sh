64bbba8a136546d60e3c55e0a3ec49e1db2c6d70
Yun Teng <yteng@thoughtworks.com>
2015-05-02 01:48:27 +0800

    kill first

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+
+PID=$(ps aux | grep java | grep guodao212 | grep -v grep | awk '{ print $2 }')
+if [ -e "$PID" ]; then
+   echo "Killing exiting running guodao212 process with PID [${PID}]"
+   kill -9 $PID
+fi
+
 export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
 MYDIR=$(dirname $0)
 cd $MYDIR
-./gradlew clean jettyRun 
+./gradlew clean jettyRun
