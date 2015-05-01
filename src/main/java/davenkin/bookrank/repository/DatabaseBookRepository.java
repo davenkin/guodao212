d8d28bcd535391e51335426732ceb09384b98912
Yun Teng <yteng@thoughtworks.com>
2015-05-01 20:59:28 +0800

    add log

--- src/main/java/davenkin/bookrank/repository/DatabaseBookRepository.java
+++ src/main/java/davenkin/bookrank/repository/DatabaseBookRepository.java
@@ -11,16 +11,13 @@ import java.util.List;
  * Created by twer on 5/1/14.
  */
 public class DatabaseBookRepository implements BookRepository {
-    private final Logger logger = Logger.getLogger(this.getClass());
 
     public static final String SELECT_SQL = "SELECT BOOK.BOOK_ID,BOOK.BOOK_NAME,BOOK.BOOK_PAGE_URL,BOOK.CATEGORY_ID,CATEGORY.MERCHANT,CATEGORY.CATEGORY_NAME,CATEGORY.DESCRIPTION,CATEGORY.URL_TEMPLATE,CATEGORY.MAX_NUMBER FROM RANK_BOOK BOOK,CATEGORY CATEGORY WHERE BOOK.CATEGORY_ID=CATEGORY.CATEGORY_ID;";
     private JdbcTemplate jdbcTemplate;
 
     @Override
     public List<RankBook> allBooks() {
-        List<RankBook> rankBooks = jdbcTemplate.query(SELECT_SQL, new RankBookRowMapper());
-        logger.info("=============Found "+rankBooks.size()+" books to report=========");
-        return rankBooks;
+        return jdbcTemplate.query(SELECT_SQL, new RankBookRowMapper());
     }
 
     public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
